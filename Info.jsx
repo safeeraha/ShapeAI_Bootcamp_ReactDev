ec7f5f362c6ff417c2b4a70dc78223e597de6589
safeer ahamed <68419322+safeeraha@users.noreply.github.com>
2021-06-26 17:53:48 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Web Dev with Javascript and React.js</h1>
+        <p>
+          Here we bring our another free 7 days live BOOTCAMP with a hands-on
+          project at the end - The Foundation of Javascript and Backend with
+          NodeJS
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Web Dev with Python</h1>
+        <p>
+          Here we bring our another free 7 days live BOOTCAMP with a hands-on
+          project at the end - The Foundation of Javascript and Backend with
+          NodeJS
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Test Automation with Selenium</h1>
+        <p>
+          Here we bring our another free 7 days live BOOTCAMP with a hands-on
+          project at the end - The Foundation of Javascript and Backend with
+          NodeJS
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Manual Testing with Scratch</h1>
+        <p>
+          Here we bring our another free 7 days live BOOTCAMP with a hands-on
+          project at the end - The Foundation of Javascript and Backend with
+          NodeJS
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
